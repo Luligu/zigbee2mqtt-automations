e78452b1d9cb452c7bbbd6e936b261c804847547
Luligu <target.epithet-0h@icloud.com>
2024-11-29 22:55:14 +0100

    Release 1.0.10

--- src/automations.ts
+++ src/automations.ts
@@ -240,6 +240,7 @@ class AutomationsExtension {
     this.triggerForTimeouts = {};
     this.turnOffAfterTimeouts = {};
     this.automationsTopic = 'zigbee2mqtt-automations';
+    // eslint-disable-next-line no-useless-escape
     this.topicRegex = new RegExp(`^${this.automationsTopic}\/(.*)`);
 
     this.logger.info(`[Automations] Loading automation.js`);
